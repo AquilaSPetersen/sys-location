a370b3b2e2a4674abdc16f81be279c853c1ae6f7
Lucas Anjos <banjoslucas@gmail.com>
2018-02-02 20:46:44 -0200

    Criação da Classe Imovel
    Criação da CLasse Imovel

---
+++ uimovel.pas
@@ -0,0 +1,18 @@
+unit uImovel;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils;
+type
+  TImovel = class
+    published
+      property Codigo: Integer;
+  end;
+
+implementation
+
+end.
+

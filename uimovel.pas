42da0ce138f29111bb00167905734b80ea506e0e
Áquila Scheidegger Petersen <aquilaspetersen@gmail.com>
2018-02-06 22:13:44 -0200

    modificacoes

--- uimovel.pas
+++ uimovel.pas
@@ -7,13 +7,35 @@ interface
 uses
   Classes, SysUtils;
 type
+
+  { TImovel }
+
   TImovel = class
+  private
+    FCodigo: Integer;
+    FNumero_Apartamento: integer;
+    procedure SetCodigo(AValue: Integer);
+    procedure SetNumero_Apartamento(AValue: integer);
     published
-      property Codigo: Integer;
-      property Numero_Apartamento;
+      property Codigo: Integer read FCodigo write SetCodigo;
+      property Numero_Apartamento : integer read FNumero_Apartamento write SetNumero_Apartamento;
   end;
 
 implementation
+
+{ TImovel }
+
+procedure TImovel.SetCodigo(AValue: Integer);
+begin
+  if FCodigo=AValue then Exit;
+  FCodigo:=AValue;
+end;
+
+procedure TImovel.SetNumero_Apartamento(AValue: integer);
+begin
+  if FNumero_Apartamento=AValue then Exit;
+  FNumero_Apartamento:=AValue;
+end;
 
 end.
 
